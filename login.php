c6ccf86d9d7b70efadccc68877f98fc49c218153
HyeJungHan <165731075+HyeJungHan@users.noreply.github.com>
2024-04-05 15:59:09 +0900

    040524

--- login.php
+++ login.php
@@ -1,9 +1,8 @@
 <?php
     include 'connection.php';
+    session_start();
 
     if(isset($_POST['submit-btn'])) {
-        $filter_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-        $name = mysqli_real_escape_string($conn, $filter_name);
 
         $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
         $email = mysqli_real_escape_string($conn, $filter_email);
@@ -11,22 +10,24 @@
         $filter_password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
         $password = mysqli_real_escape_string($conn, $filter_password);
 
-        $filter_cpassword = filter_var($_POST['cpassword'], FILTER_SANITIZE_STRING);
-        $cpassword = mysqli_real_escape_string($conn, $filter_cpassword);
-
         $select_user = mysqli_query($conn, "SELECT * FROM 'users' WHERE email = '$email'") or die('query failed');
 
         if(mysqli_num_rows($select_user)>0) {
-            $message[] = 'user already exist';
-        }
-        else{
-            if($password != $cpassword) {
-                $message[] = 'wrong password';
+            $row = mysqli_fetch_assoc($select_user);
+            if($row['user_type']== 'admin') {
+                $_SESSION['admin_name'] = $row['name'];
+                $_SESSION['admin_email'] = $row['email'];
+                $_SESSION['admin_id'] = $row['id'];
+                header('location:admin_pannel.php');
+            }
+            else if($row['user_type']== 'user') {
+                $_SESSION['user_name'] = $row['name'];
+                $_SESSION['user_email'] = $row['email'];
+                $_SESSION['user_id'] = $row['id'];
+                header('location:index.php');
             }
             else{
-                mysqli_query($conn, "INSERT INTO 'users'('name', 'email', 'password') VALUES ('$name', '$email', '$password')") or die('query failed');
-                $message[] = 'registered successfully';
-                header('location: login.php');
+                $message[] = 'incorrect email or password';
             }
         }
     }
@@ -37,6 +38,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--box icon link-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Register page</title>
 </head>
